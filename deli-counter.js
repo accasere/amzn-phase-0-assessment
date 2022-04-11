f7190e7b2d61d11dc414544ac03adda983ab4cd4
Antony Caseres <accasere@outlook.com>
2022-04-11 03:53:13 -0500

    Assement

--- deli-counter.js
+++ deli-counter.js
@@ -1,9 +1,26 @@
 // 1. Write your functions here
+const katzDeli = []
+
+
+function takeANumber (array,customerName){
+    // Adding new name to the end of the array
+    array.push(customerName)
+    for (let [index, customer] of array.entries()){
+        if (customer === customer.name){
+            console.log(`Welcome, ${customerName}. You are number ${index} in line.`)
+        }
+    }
+ }
+
+
+
+
+ 
 
 
 // 2. Example Usage
 
-// const katzDeli = []
+//const katzDeli = []
 
 // takeANumber(katzDeli, "Ada") //=> Welcome, Ada. You are number 1 in line.
 // takeANumber(katzDeli, "Grace") //=> Welcome, Grace. You are number 2 in line.
